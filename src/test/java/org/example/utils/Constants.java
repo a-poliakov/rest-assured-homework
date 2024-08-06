45f4a16e15b6587565c61179929b9563b6ae7c10
Andrei Poliakov <avpgenium@yandex.ru>
2024-08-06 17:26:10 +0300

    sample test and project structure

--- src/test/java/org/example/utils/Constants.java
+++ src/test/java/org/example/utils/Constants.java
@@ -1,7 +1,7 @@
 package org.example.utils;
 
 public interface Constants {
-    String BASE_URL = "http://backend:9966";
+    String BASE_URL = "http://localhost:9966";
     int CLIENT_ERROR_CODE = 503;
     int SUCCESS_CODE = 200;
     String POS_API_SCHEMA_ERROR_JSON = "";
