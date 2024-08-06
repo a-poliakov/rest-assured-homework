e227ddd113210f5d8485673883ec967ed67f8189
Andrei Poliakov <avpgenium@yandex.ru>
2024-08-06 15:06:16 +0300

    initial commit

---
+++ src/test/java/org/example/SimpleTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @Test
+    void testStub() {
+        Assertions.assertTrue(true);
+    }
+}
